82d98acc0b75cd35eb830786ad3c635f28fcc5a1
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-10-03 09:10:04 +0300

    fix home img bug

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             </div>
 
             <div className={styles.imgContainer}>
-                <img className={styles.img} src="/blue-filmstrip-png-21.png" alt="image" />
+                <img className={styles.img} src="./homeIMG.png" alt="image" />
             </div>
         </div>
     )
